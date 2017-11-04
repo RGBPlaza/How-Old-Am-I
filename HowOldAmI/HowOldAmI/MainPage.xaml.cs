01f0bc9fe58a82b32e06fe0a918b692853d7addd
Joshua Yarrow <rayred14@live.com>
2017-11-04 01:54:30 +0000

    This will do for now

---
+++ HowOldAmI/HowOldAmI/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HowOldAmI
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            var currentView = SystemNavigationManager.GetForCurrentView();
+            currentView.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+        }
+
+        private void DatePicker_SelectedDatesChanged(CalendarView sender, CalendarViewSelectedDatesChangedEventArgs args)
+        {
+            // If a new date has been selected
+            if (args.AddedDates.Count > 0)
+            {
+                DateTime selectedDate = args.AddedDates.First().Date;
+                if (selectedDate.CompareTo(DateTime.Now.Date) < 0)                                                          // If the selected date is earlier than today,
+                    Info.DateDifference = DateTime.Now.Date.Subtract(selectedDate);                                              // Calculate the date difference
+                SubmitButton.IsEnabled = args.AddedDates.First().Date.CompareTo(DateTime.Now.Date) < 0 ? true : false;      // Enable the submit button or Disable if selected date is later
+            }
+            else                                                                                                            // If the date has been de-selected,
+                SubmitButton.IsEnabled = false;                                                                             // Disable the submit button
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(ResultPage));
+        }
+    }
+
+    public static class Info
+    {
+        public static TimeSpan DateDifference;
+    }
+}
---
+++ HowOldAmI/HowOldAmI/ResultPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Core;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace HowOldAmI
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ResultPage : Page
+    {
+        public ResultPage()
+        {
+            this.InitializeComponent();
+
+            // GUI Stuff
+            var currentView = SystemNavigationManager.GetForCurrentView();
+            currentView.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            currentView.BackRequested += (ev, x) => { if (Frame.CanGoBack) Frame.GoBack(); };
+            SizeChanged += (s, e) => ValuesStackPanel.Orientation = e.NewSize.Height >= e.NewSize.Width ? Orientation.Vertical : Orientation.Horizontal;
+            Transitions = new Windows.UI.Xaml.Media.Animation.TransitionCollection() { new Windows.UI.Xaml.Media.Animation.NavigationThemeTransition() };
+
+            // Calculate rough number of years
+            int years = (int)Math.Floor(Info.DateDifference.TotalDays / 365.2425);
+            // Calculate rough number of residual months
+            int months = (int)Math.Floor((Info.DateDifference.TotalDays % 365.25) / 30.4375);
+            // Calculate rough number of residual days
+            int days = (int)Math.Floor((Info.DateDifference.TotalDays % 365.25) % 30.4375);
+
+            // Display Information Appropriately
+            YearsTextBlock.Text = years == 1 ? $"{years.ToString()} Year," : $"{years.ToString()} Years,";
+            MonthsTextBlock.Text = months == 1 ? $"{months.ToString()} Month," : $"{months.ToString()} Months,";
+            DaysTextBlock.Text = days == 1 ? $"{days.ToString()} Day" : $"{days.ToString()} Days";
+        }
+    }
+}
